25f360fd22abeb960b71a1bf99c860ef3248cb8c
cooldoggop <46602606+cooldoggop@users.noreply.github.com>
2022-11-27 04:39:34 +0500

    Add files via upload

--- Lab_6.java
+++ Lab_6.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.Math.*;
 
@@ -53,42 +52,34 @@ public class Lab_6 {
        return month;
     }
     public static int num3_1(){
-        int f = 0;
         int sum = 0;
         for (int i = 1; i <= 8; i++){
-            for (int j = 1; j <= i; j++){
-                sum += pow((j + i),2);
+            for (int j = 1; j <= i; j++) {
+                sum += pow((j + i), 2);
             }
-            f += sum;
         }
-        return f;
+        return sum;
     }
     public static int num3_2(){
-        int f =1;
         int umnozh = 1;
         for (int i = 1; i <=5; i++){
             for (int j = 1; j <= i; j++){
                 umnozh *= j;
             }
-            f *= umnozh;
         }
-        return f;
+        return umnozh;
     }
     public static double num3_3(){
-        int i,j,k;
-        double f = 1;
-        double umnozh = 0;
-        double s = 0;
-        for (i = 1; i <= 8; i++){
-           for (j = 1; j <= 2*i-1; j++){
-               for (k = i + j; k <= 2*(i+j); k++){
-                   s = 2*j-3*(i-0.5*k);
-               }
-               umnozh += s;
-           }
-           f *= umnozh;
+        double i,j,k,a = 0,b = 0,f=1;
+        for (i=1;i<=8;i++){
+            for(j=i;j<=2*i-1;j++){
+                for (k=i+j;k<=2*(i+j);k++){
+                    a += (2*j-3*(i-0.5*k));
+                }
+                b += a;
+            }
+            f *= b;
         }
-        f = 0;
         return f;
     }
     public static void num4(double a, int sc){
@@ -135,7 +126,7 @@ public class Lab_6 {
     public static void num9(double x, double p){
         int n = 0;
         double y0 = exp(log(x*(p+1))/p)*0.9;
-        for (double e = 0.01; e != 0.000001; e *= 0.1) {
+        for (double e = 0.01; e > 0.000001; e = e*0.1) {
             nutonfunc(e,x,y0,p,n);
         }
     }
@@ -154,7 +145,7 @@ public class Lab_6 {
     //ending of methods
     //Body of the program
     public static void main(String[] args) {
-        System.out.println("Введите номер задачи от 1 до 9");
+        System.out.println("Введите номер задачи от 1 до 10");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         int n,k,x;
@@ -168,10 +159,10 @@ public class Lab_6 {
                 System.out.println(num1_2(x,m));
                 break;
             case 2:
-                System.out.println("Input n (PC) and k (Zp)");
-                n = in.nextInt();
-                k = in.nextInt();
-                System.out.println(num2(n,k) + " Months need to buy pc");
+                    System.out.println("Input n (PC) and k (Zp)");
+                    n = in.nextInt();
+                    k = in.nextInt();
+                    System.out.println(num2(n,k) + " Months need to buy pc");
                 break;
             case 3:
                 System.out.println("f1 = " + num3_1());
@@ -185,9 +176,10 @@ public class Lab_6 {
                 while (ask == 1){
                     System.out.println("Input sc");
                     int sc = in.nextInt();
-                    if (sc >=2 && sc <= 10){
+                    if (sc >=2 && sc <= 10 && a<1 && a>0){
                         num4(a,sc);
                     }
+                    else {System.out.println("Not correct input");}
                     System.out.println("Do u want to change sc 1-yes, else - number no");
                     ask = in.nextInt();
                 }
